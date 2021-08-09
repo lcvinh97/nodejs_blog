10bf0e0830ffca817a9428fd0ee1c195b6da8e8d
Vinh Luong <vinhluong@kms-technology.com>
2021-08-09 19:21:09 +0700

    route and controllers

--- src/index.js
+++ src/index.js
@@ -2,30 +2,38 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const route = require('./routes');
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-console.log("PATH: ", path.join(__dirname, 'public'))
+// console.log("PATH: ", path.join(__dirname, 'public'))
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+); // gửi dữ liệu từ browser lên server dưới dạng html request
+app.use(express.json()); // gửi dữ liệu từ browser lên server dưới dạng javascript
+
+// những library trong js để submit: XMLHttpRequest, fetch, axios, AJAX.
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
-// template enginee
-app.engine('hbs', exphbs({
-  extname: '.hbs'
-}));
+// Template enginee
+app.engine(
+  'hbs',
+  exphbs({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', function (req, res) {
-  res.render('home');
-});
-
-app.get('/news', function (req, res) {
-  res.render('new');
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
